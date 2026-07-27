5aea05cfe2b64542a8d3eb743924cbac3d055c91
a7hu-15 <a7hu-15@users.noreply.github.com>
2026-07-28 00:13:40 +0530

    refactor(scoring): Non-Linear Master Scoring & Enterprise Algorithm Refactor

--- core/jest.config.js
+++ core/jest.config.js
@@ -6,6 +6,10 @@ const createJestConfig = nextJest({
 
 const customJestConfig = {
   testEnvironment: 'node',
+  testMatch: ['<rootDir>/__tests__/**/*.test.ts'],
+  transformIgnorePatterns: [
+    '/node_modules/(?!(@react-pdf/renderer|@react-pdf/primitives)/)',
+  ],
   moduleNameMapper: {
     '^@kubernetes/client-node$': '<rootDir>/__mocks__/@kubernetes/client-node.js',
   },
